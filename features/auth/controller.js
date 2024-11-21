0131ef16d29702d0b45dae575872f17cf9aa146b
omejece <omejece@gmail.com>
2024-11-21 14:43:49 +0000

    configured database

--- features/auth/controller.js
+++ features/auth/controller.js
@@ -11,6 +11,7 @@ async function login(req,res,next){
      try{
 
         const result = await User.findOne({where:{email:req.body.email}});
+        res.status(200).send('djdkddldldldldl')
         if(result){
            const isPasswdMatch = await bcrypt.compare(req.body.password,result.password);
            if(isPasswdMatch){

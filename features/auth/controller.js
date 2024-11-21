7e9c038fc46cefbe043d8f7b8d9250da6897964f
omejece <omejece@gmail.com>
2024-11-21 14:59:45 +0000

    configured database

--- features/auth/controller.js
+++ features/auth/controller.js
@@ -34,6 +34,7 @@ async function login(req,res,next){
         }
      }
      catch(err){
+        res.send(err)
         console.log(err);
         console.log(err.statusCode);
         let error = {status: 500, message:"server error"};

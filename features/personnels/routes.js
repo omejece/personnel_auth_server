050b76e1416495cc8fcf378920620097c73d38b7
omejece <omejece@gmail.com>
2024-11-23 11:06:13 +0000

    configured database

--- features/personnels/routes.js
+++ features/personnels/routes.js
@@ -4,15 +4,15 @@ const authController = require("../auth/controller");
 const controller = require("./controller");
 
 router.post('/authenticate',controller.authenticate);
-router.post("/",authController.verify,controller.create);
-router.put("/:id",authController.verify,controller.update);
-router.put("/image",authController.verify,controller.changeImage);
-router.put("/password",authController.verify,controller.changePassword);
+router.post("/",controller.create);
+router.put("/:id",controller.update);
+router.put("/image",controller.changeImage);
+router.put("/password",controller.changePassword);
 router.get("/password-reset-request/:email",controller.passwordResetRequest);
 router.get("/password-reset/:email",controller.resetPassword);
-router.delete("/:id",authController.verify,controller.remove);
-router.get("/:id",authController.verify,controller.read);
-router.get("/",authController.verify,controller.readAll);
+router.delete("/:id",controller.remove);
+router.get("/:id",controller.read);
+router.get("/",controller.readAll);
 
 
 
